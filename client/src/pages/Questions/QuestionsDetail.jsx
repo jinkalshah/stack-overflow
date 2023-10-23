27cd0cd32de578482cf0d272f6b76fa54ab0eb33
bvshah91 <jinkalshah98@gmail.com>
2023-10-23 12:53:04 +0530

    changed server URL

--- client/src/pages/Questions/QuestionsDetail.jsx
+++ client/src/pages/Questions/QuestionsDetail.jsx
@@ -75,7 +75,7 @@ const QuestionsDetail = () => {
     const Navigate= useNavigate()
     const dispatch= useDispatch()
     const location= useLocation()
-    const url= 'http://localhost:3000'
+    const url= 'https://stack-over-flow-server.onrender.com'
 
     const handlePosAns = (e, answerLength) =>{
         e.preventDefault()
